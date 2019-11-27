f5fbd29cd0c08eee5bd08ee69814b27259702285
smartDevel <57102149+smartDevel@users.noreply.github.com>
2019-11-27 10:53:21 +0100

    init-commit

---
+++ wp-smart-rate.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: WP Smart Rating
+Plugin URI: https://ways4.eu
+Description: Add Ratings to your post or Page with a shortcode
+Version: 1.0
+Author: Herbert Sablotny
+Author URI: https://ways4.eu
+Text Domain: wp-smart-rate
+*/
