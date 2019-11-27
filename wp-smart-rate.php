abe5779e5fb9c9a66f03bfefe1e29cb74b6b7acb
smartDevel <57102149+smartDevel@users.noreply.github.com>
2019-11-27 14:19:06 +0100

    init-function-wp_smart_rate_add_post_type

--- wp-smart-rate.php
+++ wp-smart-rate.php
@@ -7,4 +7,58 @@ Version: 1.0
 Author: Herbert Sablotny
 Author URI: https://ways4.eu
 Text Domain: wp-smart-rate
+Domain Path: /languages
 */
+// Register Custom Post Type
+function wp_smart_rate_add_post_type() {
+
+	$labels = array(
+		'name'                  => _x( 'SmartRatings', 'Post Type General Name', 'wp-smart-rate' ),
+		'singular_name'         => _x( 'SmartRating', 'Post Type Singular Name', 'wp-smart-rate' ),
+		'menu_name'             => __( 'SmartRatings', 'wp-smart-rate' ),
+		'name_admin_bar'        => __( 'SmartRatings', 'wp-smart-rate' ),
+		'archives'              => __( 'Item Archives', 'wp-smart-rate' ),
+		'attributes'            => __( 'Item Attributes', 'wp-smart-rate' ),
+		'parent_item_colon'     => __( 'Parent Item:', 'wp-smart-rate' ),
+		'all_items'             => __( 'All Items', 'wp-smart-rate' ),
+		'add_new_item'          => __( 'Add New Item', 'wp-smart-rate' ),
+		'add_new'               => __( 'Add New', 'wp-smart-rate' ),
+		'new_item'              => __( 'New Item', 'wp-smart-rate' ),
+		'edit_item'             => __( 'Edit Item', 'wp-smart-rate' ),
+		'update_item'           => __( 'Update Item', 'wp-smart-rate' ),
+		'view_item'             => __( 'View Item', 'wp-smart-rate' ),
+		'view_items'            => __( 'View Items', 'wp-smart-rate' ),
+		'search_items'          => __( 'Search Item', 'wp-smart-rate' ),
+		'not_found'             => __( 'Not found', 'wp-smart-rate' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'wp-smart-rate' ),
+		'featured_image'        => __( 'Featured Image', 'wp-smart-rate' ),
+		'set_featured_image'    => __( 'Set featured image', 'wp-smart-rate' ),
+		'remove_featured_image' => __( 'Remove featured image', 'wp-smart-rate' ),
+		'use_featured_image'    => __( 'Use as featured image', 'wp-smart-rate' ),
+		'insert_into_item'      => __( 'Insert into item', 'wp-smart-rate' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this item', 'wp-smart-rate' ),
+		'items_list'            => __( 'Items list', 'wp-smart-rate' ),
+		'items_list_navigation' => __( 'Items list navigation', 'wp-smart-rate' ),
+		'filter_items_list'     => __( 'Filter items list', 'wp-smart-rate' ),
+	);
+	$args = array(
+		'label'                 => __( 'SmartRating', 'wp-smart-rate' ),
+		'description'           => __( 'Add Ratings to your post or Page with a shortcode', 'wp-smart-rate' ),
+		'labels'                => $labels,
+		'supports'              => array(),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'post',
+	);
+	register_post_type( 'wp_smart_rate', $args );
+
+}
+add_action( 'init', 'wp_smart_rate_add_post_type', 0 );
