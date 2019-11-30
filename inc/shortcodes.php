2375fb38dde529b9ec58325663d190f04cdb6d7b
smartDevel <57102149+smartDevel@users.noreply.github.com>
2019-11-30 18:46:50 +0100

    shortcodes-css-js-styles-added

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -3,10 +3,20 @@
 //zunächst prüfen ob die Wordpress- Konstante ABSPATH gesetzt ist, sonst unbefugter (wordpress-fremder) Zugriff und exit
 if (!defined( 'ABSPATH')) exit;
 add_shortcode( 'wp_smart_rate', 'wp_smart_rate_display' );
+
 function wp_smart_rate_display ($atts, $content = null) {
-$str_output = 'mein erster shortcode in wordpress: ';
-$str_output .= '<strong>' . $content . '</strong>';
-return $str_output;
+$post_id = $atts['id'];
+$html_output = '';
+$html_output .= '
+                    <div class="smart-rate-container">
+                        <div class="smart-rate-title-bg">
+                            <div class="smart-rate-title">
+                            ' . esc_html(get_post_meta( $post_id, 'wp_smart_rate_title', true )) . '
+                            </div>
+                        </div>
+                    </div>
+';
+return $html_output;
 }
 
 ?>
